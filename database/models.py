2c223845b07d18d402246ba22c19d980865af384
msguttman <msguttman@gmail.com>
2020-12-15 00:39:09 -0600

    user model

--- database/models.py
+++ database/models.py
@@ -1,7 +1,27 @@
 from .db import db
 
 class Goal(db.Document):
-    description = db.StringField(required=True, unique=True)
-    categories = db.ListField(db.StringField(), required=True)
-    start = db.StringField(required=True, unique=True)
-    finish = db.StringField(required=True, unique=True)
+    description = db.StringField(required=True)
+    categories = db.ListField(db.StringField())
+    start = db.DateTimeField(required=True, default=datetime.datetime.utcnow)
+    finish = db.DateTimeField(required=True, default=datetime.datetime.utcnow)
+
+class User(db.Document):
+    email = db.StringField(required=True, unique=True)
+    password = db.StringField()
+    authenticated = BooleanField(default=False)
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False

4a928160b5808df73ea9d31e7f0c4f375d6c41b0
msguttman <msguttman@gmail.com>
2020-12-28 23:46:58 -0600

    small changes

--- bricktime/auth.py
+++ bricktime/auth.py
@@ -3,3 +3,10 @@ from flask import Blueprint
 auth_bp = Blueprint(
     'auth.bp', __name__,
 )
+
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+
+@auth_bp.route('/signup', methods=['GET', 'POST'])
+def signup():
--- bricktime/routes.py
+++ bricktime/routes.py
@@ -12,20 +12,20 @@ def user_loader(email):
     """
     return User.objects(email)
 
-@app.route('/goals', methods=['GET'])
+@app.route('/goals', methods=['GET', 'POST'])
 def get_goals():
-    print("starting get_goals")
-    goals = Goal.objects().to_json()
-    return Response(goals, mimetype="application/json", status=200)
+    if methods == 'GET':
+        print("starting get_goals")
+        goals = Goal.objects().to_json()
+        return Response(goals, mimetype="application/json", status=200)
 
-
-@app.route('/goals', methods=['POST'])
-def add_goal():
-    print("starting add_goal")
-    body = request.get_json()
-    goal = Goal(**body).save()
-    id = goal.id
-    return {'id': str(id)}, 200
+   elif methods == 'POST':
+        print("starting add_goal")
+        body = request.get_json()
+        goal = Goal(**body).save()
+        id = goal.id
+        return {'id': str(id)}, 200
+    
 
 @app.route('/goals/<id>')
 def get_goal(id):
